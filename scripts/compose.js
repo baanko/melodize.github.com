a2aba934faf8eaba7473a0569995ab9347b0115e
jhw <hyoungwookjin123@gmail.com>
2017-10-26 14:47:39 +0900

    compose sound included

--- scripts/compose.js
+++ scripts/compose.js
@@ -5,11 +5,23 @@ var preferrence;
 var album;
 var lyrics;
 var instrument;
+var sound = [new Audio("./sounds/do.wav"),
+			 new Audio("./sounds/rae.wav"),
+			 new Audio("./sounds/mi.wav"),
+			 new Audio("./sounds/fa.wav"),
+			 new Audio("./sounds/sol.wav"),
+			 new Audio("./sounds/ra.wav"),
+			 new Audio("./sounds/si.wav"),
+			];
+var song = [];
+var start = 0;
+var end = 10;
+var playing = false;
 
 function fillScore(length){
 	var code = "";
     for(var i = 0; i < length; i++){
-    	code+=  "<div style='border: 1px solid; height: 300px; width:50px; display: inline-block'>"+
+    	code+=  "<div id='column"+i+"'style='border: 1px solid; height: 300px; width:50px; display: inline-block'>"+
     				"<div id='note' x_cor='"+i+"' y_cor= '5' style='border: 1px solid; height: 50px; width: 50px'></div>"+
             		"<div id='note' x_cor='"+i+"' y_cor= '4' style='border: 1px solid; height: 50px; width: 50px'></div>"+
             		"<div id='note' x_cor='"+i+"' y_cor= '3' style='border: 1px solid; height: 50px; width: 50px'></div>"+
@@ -25,7 +37,47 @@ $(document).on('click', '#note', function(){
 	var x_cor = this.getAttribute("x_cor");
 	var y_cor = this.getAttribute("y_cor");
 	this.style.backgroundColor = "black";
-	console.log(x_cor+" "+y_cor);
+	var note = sound[y_cor].cloneNode();
+	note.play();
+	note.remove();
+	song[x_cor] = y_cor;
+});
+
+function playSong(start, end){
+	if(start > end){
+		var temp = start;
+		start = end;
+		end = temp;
+	}
+	var i = start;
+	var countdown = setInterval(function(){
+		if(i == end){
+			clearInterval(countdown);
+			playing = false;
+			$("#column"+(i-1)).removeClass("currCol");
+		}
+		else{
+			$("#column"+(i-1)).removeClass("currCol");
+			$("#column"+i).addClass("currCol");
+			if(song[i] == undefined){
+				i++;
+				return;
+			}
+			var note = sound[song[i]].cloneNode();
+			note.play();
+			note.remove();
+		}
+		i++;
+	}, 500);
+};
+
+$("#playSong").on('click', function(){
+	if(playing == false){
+		playing = true;
+		var st = $("#from").val();
+		var ed = $("#to").val();
+		playSong(st, ed);
+	}
 });
 
 function init(){
